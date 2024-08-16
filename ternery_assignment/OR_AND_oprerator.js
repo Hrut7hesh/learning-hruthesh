733d191c9614b80b039d20173b75afa989a60217
Hruthesh <127265328+Hrut7hesh@users.noreply.github.com>
2024-08-16 18:44:34 +0530

    Delete ternery_assignment directory

--- ternery_assignment/OR_AND_oprerator.js
+++
@@ -1,8 +0,0 @@
-// console.log(1 && 2 && 1);
-// console.log(null || undefined || null);
-// console.log('hello' && true && 'hello');
-const person = {
-    name: 'John'
-};
-console.log(person.name || 'Unknown');
-console.log(person.job || 'Unknown'); 
