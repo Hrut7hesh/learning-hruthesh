77708f4f764ac4cb49c83280285b68a7f87d7b75
Hrut7hesh <gelle.hrutheshredd@gmail.com>
2024-08-25 23:30:12 +0530

    Redux todo task completed

---
+++ redux_todo/src/App.jsx
@@ -0,0 +1,11 @@
+import Task from './components/Task'
+import TaskList from './components/TaskList';
+function App() {
+return (
+<div className="App">
+<Task />
+<TaskList />
+</div>
+);
+}
+export default App;
